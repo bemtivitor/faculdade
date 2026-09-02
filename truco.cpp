42b58430c9377282031717732d7a492c77524ac1
Guiii <amelcoguilherme@gmail.com>
2026-09-01 21:54:23 -0300

    feat: adding mostrar carta

--- truco.cpp
+++ truco.cpp
@@ -1,67 +1,88 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 using namespace std;
 
-const int PAUS = 1;
+// Guilherme
+// criarBaralho()
+// embaralharBaralho()
+// distribuirCartas()
+// sortearCoringa()
+// mostrarMao()
+// ~mostrarCarta()~
+
+// Você
+// definirForcaCarta()
+// compararCartas()
+// jogarRodada()
+// verificarVencedorMao()
+// pedirTruco()
+// aumentarTruco()
+// atualizarPontuacao()
+// verificarFimJogo()
+
+// variáveis imutáveis reutilizadas no código
+
+const int PAUS = 3;
 const int COPAS = 2;
-const int ESPADAS = 3;
-const int OURO = 4;
+const int ESPADAS = 1;
+const int OURO = 0;
+const char *NAIPES[4] = {
+    "♦",
+    "♠",
+    "♥",
+    "♣"};
+const char VALORES[13] = "456789QJKA23";
 
-struct carta{
-    int valor;
-    int naipe;
-   };
+struct Carta
+{
+    int valor, naipe;
+};
 
-void mostrarNaipe (int naipe){
-        if (naipe == 1)
-        {
-            cout << "Paus" << endl;
-        }
-        else if (naipe == 2)
-        {
-            cout << "Copas" << endl;
-        }
-        else if (naipe == 3)
-        {
-            cout << "Espadas" << endl;
-        }
-        else if (naipe == 4)
-        {
-            cout << "Ouro" << endl;
-        }
-        
-    }
+struct Jogador
+{
+    Carta mao[3];
+};
 
-
-    // P = 1 
-    // C = 2 
-    // E = 3  
-    // O = 4       
-
-    //4  = 4
-    //5  = 5
-    //6  = 6
-    //7  = 7
-    //Q  = 8
-    //J  = 9
-    //K  = 10
-    //A  = 11
-    //2  = 12
-    //3  = 13
-
-
-int main(){
-   carta carta1;
-
-        carta1.valor = 4;
-        carta1.naipe = PAUS;
-        mostrarNaipe(carta1.naipe);
-
-
-    cout << "Carta 1: " << carta1.valor << " de ";
-    mostrarNaipe(carta1.naipe);
-
-return 0;
+char mostrarNumeroDaCarta(int valor)
+{
+    return VALORES[valor];
 }
 
- 
+const char *mostrarNaipe(int naipe)
+{
+    return NAIPES[naipe];
+}
+
+string mostrarCarta(Carta carta)
+{
+    return string(1, mostrarNumeroDaCarta(carta.valor)) + mostrarNaipe(carta.naipe);
+}
+
+// O = 1
+// E = 2
+// C = 3
+// P = 4
+
+// 4  = 0
+// 5  = 1
+// 6  = 2
+// 7  = 3
+// Q  = 4
+// J  = 5
+// K  = 6
+// A  = 7
+// 2  = 8
+// 3  = 9
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    Carta carta;
+    carta.naipe = 0;
+    carta.valor = 0;
+
+    cout << mostrarCarta(carta) << endl;
+    return 0;
+}
